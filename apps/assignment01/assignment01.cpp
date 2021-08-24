0ca76c08429e8a476408000cac3c96fb0cc38eb2
rcrossla <rcrossla@my.athens.edu>
2021-08-24 14:34:02 -0500

    completed assignment code

--- apps/assignment01/assignment01.cpp
+++ apps/assignment01/assignment01.cpp
@@ -1,15 +1,84 @@
 //
 // File:   assignment1.cpp
-// Author: <PUT YOUR NAME HERE>
-// Purpose:
+// Author: Rachel Crossland
+// Purpose: 
 // Illustrate some of the bad thins that can happen with
 // pointers
 //
 #include <iostream>
+#include <string>
+
 using namespace std;
 
+const int ARRAYSIZE = 100;
+
+void functionOne();
+void functionTwo();
+void functionThree();
+string* functionHelper();
 
 int main() {
-  // Put your code here.
-  return 0;
+
+    functionOne();
+    functionTwo();
+    functionThree();
+
+
+    return 0;
+}
+
+void functionOne() {
+
+    cout << "function one" << endl;
+
+    int* ptr1 = new int[ARRAYSIZE];
+    for (int i = 0; i < ARRAYSIZE; i++) {
+        *(ptr1 + i) = i;
+    }
+
+    int* ptr2 = ptr1;
+
+    delete[] ptr1;
+
+    for (int i = 0; i <= 9; i++) {
+        cout << *(ptr2 + i) << endl;
+    }
+
+}
+
+void functionTwo() {
+
+    cout << "function two" << endl;
+
+    int* ptr1 = new int[ARRAYSIZE];
+    for (int i = 0; i < ARRAYSIZE; i++) {
+        *(ptr1 + i) = i;
+    }
+
+    int* ptr2 = ptr1;
+
+    delete ptr1;
+
+    cout << "Address: " << ptr2 << endl;
+    for (int i = 0; i <= 9; i++) {
+        cout << *(ptr2 + i) << endl;
+    }
+}
+
+void functionThree() {
+
+    cout << "function three" << endl;
+
+    string* ptr = functionHelper();
+
+    cout << *ptr << endl;
+}
+
+string* functionHelper() {
+
+    string myString[] = { "This is my string" };
+
+    string* ptr = myString;
+
+    return myString;
 }
