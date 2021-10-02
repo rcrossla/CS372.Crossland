d0eb797988ea827b7d161dcd06a63652a7c39267
rcrossla <rcrossla@my.athens.edu>
2021-10-01 22:05:06 -0500

    friday night changes

--- apps/assignment04/assignment04.cpp
+++ apps/assignment04/assignment04.cpp
@@ -2,35 +2,48 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 using namespace std;
 
 
-void function(int);
+void function(int, vector<string>);
 int display();
+void set(string);
+
+vector<string> vec;
 
 int main()
 {
-	ofstream inputFile;
+	ifstream inputFile;
 
 	inputFile.open("tasks.txt");
-	int number = 0;
+
+	int num = 0;
 	string str;
 
-	while (getline(cin, str))
+	if (inputFile)
 	{
-		if (str.size() > 0)
+
+		while(getline(inputFile,str))
 		{
-			vector.push_back(str);
+			vec.push_back(str);
 		}
+
+		inputFile.close();
+
+		num = display();
+		function(num, vec);
 	}
-
-
-	
-
-
+	else
+	{
+		cout << "file failed";
+	}
 }
+
+
 int display()
 {
+	int number = 0;
 	do
 	{
 		cout << "Select a number: " << endl << endl;
@@ -45,52 +58,63 @@ int display()
 
 	return number;
 }
-void function(int num)
+void function(int num, vector<string> vec)
 {
 	int choice = 0;
 	string line = "";
-
-	
+		
 	switch(num)
 	{
 		case 1:
 		{
-			while (!inputFile.eof())
+			cout << "case 1 opens";
+			
+			for(int i = 0; i < vec.size(); i++)
 			{
-				cout << getline(inputFile, line) << endl;
+				
+				cout << vec[i] << endl;
 			}
+			break;
 		}
 		case 2:
 		{
-			
 			cout << "Which item would you like to view: ";
 			cin >> choice;
 
 			cout << endl << endl;
 
-			cout << vector.at(choice);
+			cout << vec[choice];
 
+			break;
 		}
 		case 3:
 		{
 			cout << "Which task is done: ";
 			cin >> choice;
 
-			if (vector.at(choice) == 'U')
-			{
-				vector.at(choice) = 'D';
-			}
+			choice -= 1;
+			line = vec[choice];
 
+			set(line);
 
+			break;
 		}
 		default: 
 		{
-
+			exit(0);
 		}
-
 	}
+}
 
-
+void set(string line)
+{
+	for (int i = 0; i < line.size(); i++)
+	{
+		if (line[i] == 'U')
+		{
+			line[i] = 'D';
+		}
+	}
 
 }
 
