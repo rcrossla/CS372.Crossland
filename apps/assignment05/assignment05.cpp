d9b11c120202f1d35b8923186555b29ef82f54cb
rcrossla <rcrossla@my.athens.edu>
2021-10-06 19:06:41 -0500

    wednesday changes

---
+++ apps/assignment05/assignment05.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+void moveDisks(int, int, int, int);
+void iterative(int, int, int, int);
+
+int main()
+{
+	const int NUMDISKS = 4;
+	const int FROMPEG = 1;
+	const int TOPEG = 3;
+	const int TEMPPEG = 2;
+	cout << "RECURSIVE" << endl << endl;
+	moveDisks(NUMDISKS, FROMPEG, TOPEG, TEMPPEG);
+	cout << "All the pegs are moved!" << endl;
+
+	cout << "-------------------------------------------------------------------------" << endl << endl;
+
+	cout << "ITERATIVE" << endl << endl;
+	iterative(NUMDISKS, FROMPEG, TOPEG, TEMPPEG);
+	cout << "All the pegs are moved!" << endl;
+
+}
+void moveDisks(int num, int fromPeg, int toPeg, int tempPeg) {
+
+	if (num > 0) {
+		moveDisks(num - 1, fromPeg, tempPeg, toPeg);
+		cout << "Move a disk from peg " << fromPeg << " to peg " << toPeg << endl;
+		moveDisks(num - 1, tempPeg, toPeg, fromPeg);
+	}
+}
+void iterative(int num, int fromPeg, int toPeg, int tempPeg) {
+
+	if (num % 2 == 0) {
+		
+		for (int i = num; i >= 0; i--) {
+			cout << "even" << endl;
+			cout << "Move a disk from peg " << fromPeg << " to peg " << tempPeg << endl;
+			cout << "Move a disk from peg " << fromPeg << " to peg " << toPeg << endl; 
+			cout << "Move a disk from peg " << tempPeg << " to peg " << toPeg << endl;
+		}
+	}
+	else{
+		for (int i = num; i >= 0; i--) {
+
+			cout << "Move a disk from peg " << fromPeg << " to peg " << toPeg << endl;
+			cout << "Move a disk from peg " << fromPeg << " to peg " << tempPeg << endl;
+			cout << "Move a disk from peg " << tempPeg << " to peg " << toPeg << endl;
+		}
+
+	}
+
+}
